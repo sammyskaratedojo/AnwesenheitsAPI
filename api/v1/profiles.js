e7660be8f7f134ee0f66383dd0bd33d622a4bcb0
sammyskaratedojo <sammyskaratedojoding@gmail.com>
2025-08-29 23:46:24 +0200

    Update profiles.js

--- api/v1/profiles.js
+++ api/v1/profiles.js
@@ -8,6 +8,14 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  return res.status(200).end();
+
+  
   if (!client) {
     client = new MongoClient(uri);
     await client.connect();
@@ -31,6 +39,7 @@ export default async function handler(req, res) {
     res.status(405).end();
   }
 }
+
 
 
 

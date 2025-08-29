44fcf4ecd23fccde3d1cc71a324608e179032826
sammyskaratedojo <sammyskaratedojoding@gmail.com>
2025-08-29 22:32:49 +0200

    Add files via upload

--- api/v1/profiles.js
+++ api/v1/profiles.js
@@ -1,7 +1,4 @@
-import { MongoClient } from 'mongodb';
-
 const uri = process.env.DB_URI; // z.B. aus Vercel Environment Variables
-const authPW = process.env.AUTH_PW
 let client;
 
 export default async function handler(req, res) {
@@ -9,16 +6,20 @@ export default async function handler(req, res) {
     client = new MongoClient(uri);
     await client.connect();
   }
-
+  
   if (req.body.authPW !== authPW)
   {
     res.status(401).end()
     return
   }
 
-  
+
+  const db = client.db('AnwesenheitDB');
+  const profiles = db.collection('profiles');
+
   if (req.method === 'GET') {
-    res.status(200).end()
+    const allProfiles = await profiles.find({}).toArray();
+    res.status(200).json(allProfiles);
   } else {
     res.status(405).end();
   }

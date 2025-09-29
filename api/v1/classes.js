d9c9bb1242c1d228c324d51910d1da07e7395849
sammyskaratedojo <sammyskaratedojoding@gmail.com>
2025-09-29 21:42:40 +0200

    Update classes.js

--- api/v1/classes.js
+++ api/v1/classes.js
@@ -5,9 +5,9 @@ const uri = process.env.DB_URI;
 let client;
 
 export default async function handler(req, res) {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
@@ -58,4 +58,5 @@ export default async function handler(req, res) {
 //     return res
 
 // }
+
 

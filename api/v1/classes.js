3dbd1f593e3be2df6006ad88c0fb993d9b33b83a
sammyskaratedojo <sammyskaratedojoding@gmail.com>
2025-09-29 21:25:59 +0200

    Update classes.js

--- api/v1/classes.js
+++ api/v1/classes.js
@@ -29,9 +29,8 @@ export default async function handler(req, res) {
 async function getClasses()
 {
     const db = client.db('AnwesenheitDB');
-    const classes = db.collection('classes');
+    const classes = db.collection('profiles');
     let allClasses = await classes.find({}).toArray()
-    return allClasses
     let res = []
     allClasses.forEach(c => {
         res.push(c.name)

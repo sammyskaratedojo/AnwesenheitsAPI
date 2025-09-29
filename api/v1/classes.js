9783bf3a01ec009446127db1f4f5b5f8707e30d6
sammyskaratedojo <sammyskaratedojoding@gmail.com>
2025-09-29 22:29:55 +0200

    Update classes.js

--- api/v1/classes.js
+++ api/v1/classes.js
@@ -5,44 +5,57 @@ const uri = process.env.DB_URI;
 let client;
 
 export default async function handler(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-    if (req.method === "OPTIONS") {
-        return res.status(200).end();
-    }
-    
-    if (req.method !== "POST") {
-        return res.status(405).json({ error: "Only POST allowed!" });
-    }
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
-    if (!client) {
-        client = new MongoClient(uri);
-        await client.connect();
-    }
+  if (!client) {
+    client = new MongoClient(uri);
+    await client.connect();
+  }
 
-    //   if (req.body.authPW !== authPW)
-    //   {
-    //     res.status(401).end()
-    //     return
-    //   }
-
-    const { date, className } = req.body;
+//   if (req.body.authPW !== authPW)
+//   {
+//     res.status(401).end()
+//     return
+//   }
 
 
-    const db = client.db('AnwesenheitDB');
-    const sessions = db.collection('sessions');
+  const db = client.db('AnwesenheitDB');
+  const profiles = db.collection('classes');
 
-    const session = await sessions.find({"session_date": date, "class_name": className}).toArray()
+  if (req.method === 'GET') {
+    const allClasses = await profiles.find({}).toArray()
+    let result = []
+    allClasses.forEach(c => {
+        result.push(c.name)
+    })
 
-    if(session.length != 0)
-    {
-        res.status(400).json({error: "Die Sitzung existiert bereits"})
-        return;
-    }
-    else
-    {
-        res.status(200).json("ok")
-    }
+    res.status(200).json(result)
+  } else {
+    res.status(405).end()
+  }
 }
+
+
+// async function getClasses()
+// {
+//     const db = client.db('AnwesenheitDB');
+//     const classes = db.collection('profiles');
+//     let allClasses = await classes.find({}).toArray()
+//     return allClasses
+//     let res = []
+//     allClasses.forEach(c => {
+//         res.push(c.name)
+//         console.log(c)
+//         console.log(c.name + " !")
+//     })
+
+//     return res
+
+// }
+

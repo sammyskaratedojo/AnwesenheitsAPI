1e9dbb4a31ac91cd7f2498d88a6c060647f6227b
sammyskaratedojo <sammyskaratedojoding@gmail.com>
2026-02-09 23:27:40 +0100

    Update zw-sessions.js

--- api/v1/zw-sessions.js
+++ api/v1/zw-sessions.js
@@ -5,7 +5,18 @@ let client;
 
 let zwDb;
 
-export default async function handler(req, res) {
+export default async function handler(req, res)
+{
+    // --- CORS ---
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	
+	if (req.method === "OPTIONS") {
+		res.status(200).end();
+		return;
+	}
+    
     
     if (req.method !== 'GET') {
         res.status(405).end()

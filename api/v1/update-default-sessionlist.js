04995acf596d7bc23eb13161afab3b5d337f0f71
sammyskaratedojo <sammyskaratedojoding@gmail.com>
2026-06-15 22:19:09 +0200

    Update update-default-sessionlist.js

--- api/v1/update-default-sessionlist.js
+++ api/v1/update-default-sessionlist.js
@@ -8,8 +8,19 @@ let zwDb;
 
 export default async function handler(req, res) {
     
-    if (req.method !== 'GET') {
-        res.status(405).end()
+    res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	
+	if (req.method === "OPTIONS") {
+		res.status(200).end();
+		return;
+	}
+
+    
+    if(req.method !== "POST")
+    {
+        res.status(405).end();
         return
     }
     
